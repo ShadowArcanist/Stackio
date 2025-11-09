c3f53f21189c098a34a39c47c64192da770b13ca
ShadowArcanist <projectshadowmoon@gmail.com>
2025-11-09 10:45:36 +0530

    [core] Fixed local font not loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/local";
-
-const inter = Inter({
-  src: "/fonts/Inter.woff2",
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -16,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className="font-inter" suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider
           search={{
