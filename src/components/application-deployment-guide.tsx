0155133ee94c8fa2c63558c8fefddcb7a7bc5e32
ShadowArcanist <projectshadowmoon@gmail.com>
2025-11-12 02:07:09 +0530

    [core] Changed location of application-deployment-guide and updated import paths

--- src/components/application-deployment-guide.tsx
+++ src/components/application-deployment-guide.tsx
@@ -2,7 +2,7 @@ import { source } from "@/lib/source";
 import { getMDXComponents } from "@/mdx-components";
 
 export function AppGuide() {
-  const page = source.getPage(["app-guide"]);
+  const page = source.getPage(["core/application-deployment-guide"]);
   if (!page) return null;
 
   const MDX = page.data.body;
